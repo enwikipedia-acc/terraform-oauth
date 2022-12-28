a955dbb0f325b66b8fea35263f59417ca435e051
Simon Walker <github@stwalkerster.co.uk>
2022-12-28 19:03:41 +0000

    Fix proxy

--- oauth.tf
+++ oauth.tf
@@ -3,7 +3,7 @@ resource "cloudvps_web_proxy" "oauth_proxy" {
   count    = min(1, length(local.oauth_endpoints))
   hostname = local.oauth_proxy_hostname
   domain   = var.oauth_proxy_domain
-  backends = local.oauth_endpoints[0]
+  backends = [local.oauth_endpoints[0]]
 }
 
 module "oauth-server-blue" {
