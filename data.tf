bfd1cea75e6a07a763c373bf30306d6f27869096
Simon Walker <github@stwalkerster.co.uk>
2022-12-28 18:47:46 +0000

    deploy blue environment based on new snapshot

--- data.tf
+++ data.tf
@@ -4,11 +4,6 @@ data "openstack_compute_flavor_v2" "small" {
   min_disk = 15
 }
 
-data "openstack_images_image_v2" "bullseye" {
-  most_recent = true
-  name        = "debian-11.0-bullseye"
-}
-
 data "openstack_networking_network_v2" "network" {
   external = false
 }
