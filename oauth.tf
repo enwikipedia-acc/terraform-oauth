5db10ee9fd2ea593460024f17293f4af789df293
Simon Walker <github@stwalkerster.co.uk>
2022-12-29 17:37:21 +0000

    Use blue/green module instead

--- oauth.tf
+++ oauth.tf
@@ -5,34 +5,47 @@ resource "cloudvps_web_proxy" "oauth_proxy" {
 }
 
 resource "cloudvps_web_proxy" "staging_oauth_proxy" {
-  count    = var.staging_instance != null ? 1 : 0
+  count    = module.bluegreen.staging_count
   hostname = local.oauth_staging_proxy_hostname
   domain   = var.oauth_proxy_domain
-  backends = ["http://${trimsuffix(local.staging_env, ".")}:80"]
+  backends = ["http://${trimsuffix(module.bluegreen.staging_dns_name, ".")}:80"]
 }
 
 resource "openstack_dns_recordset_v2" "prod_instance" {
   name    = "oauth-prod.${data.openstack_dns_zone_v2.rootzone.name}"
   zone_id = data.openstack_dns_zone_v2.rootzone.id
   type    = "CNAME"
-  records = [local.prod_env]
+  records = [module.bluegreen.live_dns_name]
   ttl     = 180
 }
 
+module "bluegreen" {
+  source  = "app.terraform.io/enwikipedia-acc/bluegreen/openstack"
+  version = "0.2.0"
+
+  blue_dns_name       = "${local.blue_resource_prefix}.${data.openstack_dns_zone_v2.rootzone.name}"
+  green_dns_name      = "${local.blue_resource_prefix}.${data.openstack_dns_zone_v2.rootzone.name}"
+  live_environment    = var.live_instance
+  staging_environment = var.staging_instance
+}
 
 module "oauth-server-blue" {
   source  = "app.terraform.io/enwikipedia-acc/mediawiki-oauth/openstack"
-  version = "0.11.0"
+  version = "0.12.0"
 
   environment = "b"
-  count       = var.live_instance == "blue" || var.staging_instance == "blue" ? 1 : 0
+  count       = module.bluegreen.blue_count
 
   database_snapshot_name = "oauth-db-20221228b"
-
   instance_type = data.openstack_compute_flavor_v2.small.id
   image_name    = "debian-11.0-bullseye"
+
+  resource_prefix = local.blue_resource_prefix
+
   network       = data.openstack_networking_network_v2.network.id
-  dns_zone      = data.openstack_dns_zone_v2.rootzone
+  dns_zone_id   = data.openstack_dns_zone_v2.rootzone.id
+  dns_name      = module.bluegreen.blue_dns_name
+
 
   security_groups = [
     openstack_networking_secgroup_v2.oauth.name,
@@ -40,23 +53,25 @@ module "oauth-server-blue" {
   ]
 
   proxy_domain    = var.oauth_proxy_domain
-  project_prefix  = var.resource_prefix
   public_hostname = "${local.oauth_proxy_hostname}.${var.oauth_proxy_domain}"
 }
 
 module "oauth-server-green" {
   source  = "app.terraform.io/enwikipedia-acc/mediawiki-oauth/openstack"
-  version = "0.10.0"
+  version = "0.12.0"
 
   environment = "g"
-  count       = var.live_instance == "green" || var.staging_instance == "green" ? 1 : 0
+  count       = module.bluegreen.green_count
 
   database_snapshot_name = "oauth-db-20221228b"
-
   instance_type = data.openstack_compute_flavor_v2.small.id
   image_name    = "debian-11.0-bullseye"
-  network       = data.openstack_networking_network_v2.network.id
-  dns_zone      = data.openstack_dns_zone_v2.rootzone
+
+  resource_prefix = local.green_resource_prefix
+
+  network     = data.openstack_networking_network_v2.network.id
+  dns_zone_id = data.openstack_dns_zone_v2.rootzone.id
+  dns_name    = module.bluegreen.green_dns_name
 
   security_groups = [
     openstack_networking_secgroup_v2.oauth.name,
@@ -64,7 +79,6 @@ module "oauth-server-green" {
   ]
 
   proxy_domain    = var.oauth_proxy_domain
-  project_prefix  = var.resource_prefix
   public_hostname = "${local.oauth_proxy_hostname}.${var.oauth_proxy_domain}"
 }
 
--- variables.tf
+++ variables.tf
@@ -39,29 +39,18 @@ variable "oauth_proxy_domain" {
 locals {
   oauth_proxy_hostname         = "${var.resource_prefix}-${var.oauth_proxy_name}"
   oauth_staging_proxy_hostname = "${var.resource_prefix}-${var.oauth_staging_proxy_name}"
-  prod_env                     = var.live_instance == "blue" ? module.oauth-server-blue[0].dns_name : module.oauth-server-green[0].dns_name
-  staging_env                  = var.staging_instance == null ? null : (var.staging_instance == "blue" ? module.oauth-server-blue[0].dns_name : module.oauth-server-green[0].dns_name)
+
+  blue_resource_prefix  = "${var.resource_prefix}-oauth-b"
+  green_resource_prefix = "${var.resource_prefix}-oauth-g"
 }
 
 # blue/green deployments
 variable "live_instance" {
   type        = string
-  default     = "blue"
   description = "The currently-active instance of the application"
-
-  validation {
-    condition     = contains(["blue", "green"], var.live_instance)
-    error_message = "The live instance must be either the 'blue' or 'green' environment"
-  }
 }
 
 variable "staging_instance" {
   type        = string
-  default     = null
   description = "The instance of the application running as a staging environment"
-
-  validation {
-    condition     = var.staging_instance == null || contains(["blue", "green"], coalesce(var.staging_instance, "none"))
-    error_message = "The staging instance must be null or either the 'blue' or 'green' environments"
-  }
 }
