f1fd6c4094dcbf37b1c1180f665a0f6c926a6171
Simon Walker <github@stwalkerster.co.uk>
2022-12-29 00:51:19 +0000

    Proper setup for blue/green

---
+++ environments.auto.tfvars
@@ -0,0 +1,5 @@
+# This file controls which environment is active at any time.
+# Valid values for the live instance are "blue" or "green"
+# Valid valies for the staging instance are "blue", "green", or null
+live_instance = "green"
+staging_instance = null
--- variables.tf
+++ variables.tf
@@ -26,14 +26,42 @@ variable "oauth_proxy_name" {
   default = "oauth"
 }
 
+variable "oauth_staging_proxy_name" {
+  type    = string
+  default = "oauth-test"
+}
+
 variable "oauth_proxy_domain" {
   type    = string
   default = "wmflabs.org"
 }
 
 locals {
-  oauth_proxy_hostname = "${var.resource_prefix}-${var.oauth_proxy_name}"
-  oauth_endpoints = [for ip in concat(module.oauth-server-blue.*.instance_ipv4, module.oauth-server-green.*.instance_ipv4) : "http://${ip}:80"]
+  oauth_proxy_hostname         = "${var.resource_prefix}-${var.oauth_proxy_name}"
+  oauth_staging_proxy_hostname = "${var.resource_prefix}-${var.oauth_staging_proxy_name}"
+  prod_env                     = var.live_instance == "blue" ? module.oauth-server-blue[0].dns_name : module.oauth-server-green[0].dns_name
+  staging_env                  = var.staging_instance == null ? null : (var.staging_instance == "blue" ? module.oauth-server-blue[0].dns_name : module.oauth-server-green[0].dns_name)
 }
 
+# blue/green deployments
+variable "live_instance" {
+  type        = string
+  default     = "blue"
+  description = "The currently-active instance of the application"
 
+  validation {
+    condition     = contains(["blue", "green"], var.live_instance)
+    error_message = "The live instance must be either the 'blue' or 'green' environment"
+  }
+}
+
+variable "staging_instance" {
+  type        = string
+  default     = null
+  description = "The instance of the application running as a staging environment"
+
+  validation {
+    condition     = var.staging_instance == null || contains(["blue", "green"], coalesce(var.staging_instance, "none"))
+    error_message = "The staging instance must be null or either the 'blue' or 'green' environments"
+  }
+}
